76b68fee2e3c8619e55fc148eb0fcf6bb7add18e
Catriona Bennett <cmb84scd@gmail.com>
2020-04-07 14:45:59 +0000

    added test to spec - non divisble by 3 or 5 number

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -5,11 +5,15 @@ describe 'fizzbuzz' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
   
-  it 'returns "buzz" when passed 5' do
+  it 'returns "buzz" when number passed divides by 5' do
     expect(fizzbuzz(5)).to eq 'buzz'
   end
   
-  it 'returns "fizzbuzz" when passed 15' do
+  it 'returns "fizzbuzz" when number passed divides by 3 and 5' do
     expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  
+  it 'returns number when number passed doesn\'t divide by 3 or 5' do
+    expect(fizzbuzz(7)).to eq 7
   end
 end
